d9e88cc8ec4b6821c372bc88ef4c8be9d922eae3
Renan Ivo <renanivom@gmail.com>
2010-11-22 02:19:09 -0200

    method redirectToAuth of ApontadorApi refactored lib

---
+++ tests/class/apontadorApiTest.php
@@ -0,0 +1,9 @@
+<?php
+$plugin_root = dirname(dirname(dirname(__FILE__))) . "/";
+require_once "PHPUnit/Framework.php";
+require_once $plugin_root . "tests/MockPress/mockpress.php";
+require_once $plugin_root . "plugin/class/apontadorApi.php";
+
+class ApontadorApiTest extends PHPUnit_Framework_TestCase {
+
+}
