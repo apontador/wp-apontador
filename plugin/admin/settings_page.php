83643f8cd0fa2f21a6ca95a8b3bf6423ef5565f1
Renan Ivo <renanivom@gmail.com>
2011-03-08 20:14:30 -0300

    settings page rearrangements

--- plugin/admin/settings_page.php
+++ plugin/admin/settings_page.php
@@ -5,14 +5,25 @@
 <?php if ($oauth_secret): ?>
 
   <p><?php _e("Authenticated as", "wp-apontador") ?></p>
-  <div >
-	  <img style="margin-right:10px;" align="left" width="64" height="64" src="<?php echo $user['user']['photo_url']; ?> ">
-    <p>
-      <strong><?php echo $user['user']['name']; ?></strong><br />
-      <?php printf(_n("%d review", "%d reviews", $user['user']['stats']['reviews'], "wp-apontador"), $user['user']['stats']['reviews']); ?><br />
-      <?php printf(_n("%d photo", "%d photos", $user['user']['stats']['photos'], "wp-apontador"), $user['user']['stats']['photos']); ?><br />
-      <?php printf(_n("%d place", "%d places", $user['user']['stats']['places'], "wp-apontador"), $user['user']['stats']['places']); ?><br />
-    </p>
+  <div id="col-container">
+    <div id="col-right">
+      <strong>Oauth Token</strong>
+      <dl>
+        <dt>Key</dt>
+        <dd><?php echo $oauth_token ?></dd>
+        <dt>Secret</dt>
+        <dd><?php echo $oauth_secret ?></dd>
+      </dl>
+    </div>
+    <div id="col-left">
+      <img style="margin-right:10px;" align="left" width="64" height="64" src="<?php echo $user['user']['photo_url']; ?> ">
+      <p>
+        <strong><?php echo $user['user']['name']; ?></strong><br />
+        <?php printf(_n("%d review", "%d reviews", $user['user']['stats']['reviews'], "wp-apontador"), $user['user']['stats']['reviews']); ?><br />
+        <?php printf(_n("%d photo", "%d photos", $user['user']['stats']['photos'], "wp-apontador"), $user['user']['stats']['photos']); ?><br />
+        <?php printf(_n("%d place", "%d places", $user['user']['stats']['places'], "wp-apontador"), $user['user']['stats']['places']); ?><br />
+      </p>
+    </div>
   </div>
 
 <?php endif; ?>
@@ -25,12 +36,12 @@
   <table class="form-table">
     <tr>
       <th colspan=2>
-        <?php _e("Getting your own Consumer Key and Consumer secret for your blog is highly recommended.", "wp-apontador") . '<BR/>'. __("Create a \"new application\" by editting your profile at apontador", "wp-apontador") .',<BR/>' . __("there you can enter your website data and get your own key and secret pair"); ?>
+        <?php _e("Getting your own Consumer Key and Consumer secret for your blog is highly recommended. Create a \"new application\" by <a href=\"http://www.apontador.com.br/accounts/app/create.html\">registering your app</a> at Apontador. There you can enter your website data and get your own key and secret pair."); ?>
       </th>
     </tr>
     <tr valign="top">
       <th scope="row">
-        <strong>Consumer Key</strong>
+        Consumer Key
       </th>
       <td>
         <input type="text" name="consumer_key" value="<?php echo $consumer_key; ?>"/>
@@ -38,31 +49,10 @@
     </tr>
     <tr valign="top">
       <th scope="row">
-        <strong>Consumer Secret</strong>
+        Consumer Secret
       </th>
       <td>
         <input type="text" name="consumer_secret" value="<?php echo $consumer_secret; ?>"/>
-      </td>
-    </tr>
-    <tr>
-      <th colspan=2>
-        <?php _e("The following codes are automagically generated, you don't need to worry about them", "wp-apontador") ?> 
-      </th>
-    </tr>
-    <tr valign="top">
-      <th scope="row">
-        <strong>OAuth Token</strong>
-      </th>
-      <td>
-        <input type="text" name="oauth_token" value="<?php echo $oauth_token ?>" />
-      </td>
-    </tr>
-    <tr valign="top">
-      <th scope="row">
-        <strong>OAuth Secret</strong>
-      </th>
-      <td>
-        <input type="text" name="oauth_secret" value="<?php echo $oauth_secret; ?>"/>
       </td>
     </tr>
   </table>
